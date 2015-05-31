9af0631b7e7f01b76eb1f632090d6b43407d0c29
Megdatronica <johnrwoolley0@gmail.com>
2015-06-01 00:35:11 +0100

    Additions to board.py

--- Python/Board.py
+++ Python/Board.py
@@ -1,8 +1,9 @@
+"""Contains the Board class"""
+
+import copy
 import Piece
 from Piece import PieceType as type
 from Piece import PieceColour as colour
-
-"""Contains the Board class"""
 
 class Board:
     """Contains a 2d array of pieces and methods for making/verifying moves.
@@ -17,6 +18,10 @@ class Board:
     def __init__(self):
         """Create clear board."""
         self.clear()
+        
+
+    def copy(self):
+        return copy.deepcopy(self)
         
         
     def clear(self):
@@ -52,36 +57,47 @@ class Board:
 
 
     def get_piece(self, x, y):
-        """Returns the piece at position x, y on the board."""
+        """Return the piece at position x, y on the board.
+        
+        Will raise IndexError if the indices are not valid.
+        """
         return self.piece_array[x][y]
     
         
     def place_piece(self, x, y, type, colour):
-        """Places a piece of the passed type at the passed location.
+        """Place a piece of the passed type at the passed location.
         
         Args:
             - x, y:  ints specifying the position on the board to place piece
             - type:  a member of the PieceType enum
             - colour:  a member of the PieceColour enum
+            
+        Will raise IndexError if the indices are not valid.
         """
         
         self.piece_array[x][y] = Piece(type, colour)
         
         
     def remove_piece(self, x, y):
-        """Removes the piece at the passed location.
+        """Remove the piece at the passed location.
         
         Args:
             - x, y:  ints specifying the position on the board to remove piece
+            
+        Will raise IndexError if the indices are not valid.
         """
         
         self.piece_array[x][y] = Piece()
         
         
     def make_move(self, move):
-        """Adjusts the state of the board to reflect the passed move."""
+        """Adjust the state of the board to reflect the passed move.
+        
+        Args:
+            - move:  a Move object
+        """
         if move.castle:
-            castle(move)
+            self.castle(move)
             
         if move.en_passant:
             self.remove_piece(*move.start_posn)
@@ -91,9 +107,61 @@ class Board:
         self.remove_piece(*move.start_posn)
         self.place_piece(*move.end_posn, piece.type, piece.colour)
 
-    def 
+
+    def is_possible_move(self, move):
+        """Return True if the passed move is possible.
+        
+        A move is POSSIBLE if it takes a piece to a square that:
+          - exists on the board, and
+          - does not contain another piece of the same colour.
+       
+        A castling move is considered POSSIBLE if the king and rook are in the
+        correct position and the space between them is empty.
+        
+        Args:
+            - move:  a Move object
+        
+        """
+        
+        if move.castle:
+            return self.is_possible_castle_move(move)
+
+        piece_moving = self.get_piece(*move.start_posn)
+        
+        try:
+            piece_at_move = self.get_piece(*move.end_posn)
+        except IndexError:
+            return False
+
+        if (piece_at_move.colour == piece_moving.colour):
+            return False
+
+        return True
 
 
+    def is_valid_move(self, move):
+        """Return true if the passed move is valid.
+        
+        A move is valid if after is it made, the king of the player who made it
+        is not in check.
+        
+        Args:
+            - move:  a Move object
+        
+        """
+        
+        if move.castle:
+            return self.is_valid_castle_move(move)
+
+        piece_moving = self.get_piece(*move.start_posn)
+        
+        new_board = self.copy()
+        new_board.make_move(move)
+
+        if newBoard.isInCheck(piece_moving.colour):
+            return false
+        
+        return true
 
 
 
